0ba602acc53bd115a6610facc4267b7d944fe718
sorines <sarah.orines@gmail.com>
2018-05-06 14:29:43 -0700

    still at 215

--- lilo.js
+++ lilo.js
@@ -3,7 +3,7 @@ var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 var fs = require("fs");
 var request = require('request');
-
+var inquirer = require('inquirer');
 
 //this holds the keys to twitter and spotify
 
@@ -13,73 +13,52 @@ var keys = require("./keys.js");
 //plant it in random.txt
 
 var nodeArgs = process.argv;
-var spotify = new spotify(keys.spotify);
-var client = new twitter(keys.twitter);
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
 
-// Create an empty variable for holding the movie name
-var songSelected = "";
+// this will transfer to the random.txt file and will cut out node and file name
+var nodeArgs = process.argv;
+var songSelected="";
 
-// Loop through all the words in the node argument
-// And do a little for-loop magic to handle the inclusion of "+"s
 for (var i = 2; i < nodeArgs.length; i++) {
 
-  if (i > 2 && i < nodeArgs.length) {
-
-    songSelected = songSelected + "+" + nodeArgs[i];
-
-  }
-
-  else {
-
-    songSelected += nodeArgs[i];
-
-  }
+  // this will just be the song selected in the random.txt
+  songSelected = songSelected + " " + nodeArgs[i];
 }
 
-//running spotify
+//this will hold everything we enter into the random.txt. It captures it. 
 
-request(spotify, function(error, response, body) {
-
-  // If the request is successful
-  if (!error && response.statusCode === 200) {
-
-    // Parse the body of the site and recover just the imdbRating
-    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-    console.log("Release Year: " + JSON.parse(body).Year);
-  }
-});
-
-
-fs.writeFile("random.txt", songSelected, function(err) {
+fs.appendFile("random.txt", "\n"+songSelected, function(err) {
 
     // If the code experiences any errors it will log the error to the console.
     if (err) {
       return console.log(err);
     }
   
-    // Otherwise, it will print: "movies.txt was updated!"
+    // Otherwise, it will print: "random.txt was updated!"
     console.log("random.txt was updated!");
   
   });
 
+//this is taken from npm spotify its suppose to print out my artist but its not doing that. fan-freaking-tastic.
 
-// importing the keys and junk
+  spotify
+  .search({ type: 'artist', type: 'album', query: 'My search query' })
+  .then(function(response) {
+    console.log(response);
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+
+// });
+
+// lets see if twitter works...
+// You can also get the stream in a callback if you prefer.
+client.get('search/tweets', {q: 'node.js'}, function(error, tweets, response) {
+  console.log(tweets);
+});
 
 
-function spotify (artist, song_name, link, album){
-    this.artist = artist;
-    this.song_name = song_name;
-    this.link = link;
-    this.album = album;
-};
-
-function twitter (tweets){
-this.tweets;
-};
-
-// var spotify = new spotify(keys.spotify);
-// var client = new twitter(keys.twitter);
 
 
-
-console.log("doesthiswork");
